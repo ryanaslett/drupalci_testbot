7b4f7eef01124a693532d769cbf7f0daed6da7a1
Jeremy Thorson <jthorson@sasktel.net>
2016-01-24 10:29:39 -0600

    2655718 by jthorson: Reformatting and adding two properties to the JobResult class

--- src/DrupalCI/Job/Results/JobResults.php
+++ src/DrupalCI/Job/Results/JobResults.php
@@ -12,34 +12,123 @@ use DrupalCI\Plugin\JobTypes\JobInterface;
 
 class JobResults {
 
+  // Tracks which stage of the build process is currently being executed
   protected $current_stage;
-  public function getCurrentStage() {  return $this->current_stage;  }
-  public function setCurrentStage($stage) {  $this->current_stage = $stage;  }
 
+  public function getCurrentStage() {
+    return $this->current_stage;
+  }
+
+  public function setCurrentStage($stage) {
+    $this->current_stage = $stage;
+  }
+
+  // Tracks the pass/fail/skipped/error status by job
+  protected $job_results;
+
+  public function getJobResults()
+  {
+    return $this->job_results;
+  }
+
+  public function setJobResults($job_results)
+  {
+    $this->job_results = $job_results;
+  }
+
+  // Tracks the pass/fail/skipped/error status by build process stage
   protected $stage_results;
-  public function getStageResults() {  return $this->stage_results;  }
-  public function setStageResults(array $stage_results) {  $this->stage_results = $stage_results;  }
-  public function getResultByStage($stage) {  return $this->stage_results[$stage];  }
-  public function setResultByStage($stage, $result) {  $this->stage_results[$stage] = $result;  }
 
+  public function getStageResults() {
+    return $this->stage_results;
+  }
+
+  public function setStageResults(array $stage_results) {
+    $this->stage_results = $stage_results;
+  }
+
+  public function getResultByStage($stage) {
+    return $this->stage_results[$stage];
+  }
+
+  public function setResultByStage($stage, $result) {
+    $this->stage_results[$stage] = $result;
+  }
+
+  // Tracks which step of the current build process stage is currently being executed
   protected $current_step;
-  public function getCurrentStep() {  return $this->current_step;  }
-  public function setCurrentStep($step) {  $this->current_step = $step;  }
 
+  public function getCurrentStep() {
+    return $this->current_step;
+  }
+
+  public function setCurrentStep($step) {
+    $this->current_step = $step;
+  }
+
+  // Tracks the pass/fail/skipped/error status for each build process step within the job
   protected $step_results;
-  public function getStepResults() {  return $this->step_results;  }
-  public function setStepResults(array $step_results) {  $this->step_results = $step_results;  }
-  public function getResultByStep($stage, $step) {  return $this->step_results[$stage][$step];  }
-  public function setResultByStep($stage, $step, $result)  {  $this->step_results[$stage][$step] = $result;  }
 
+  public function getStepResults() {
+    return $this->step_results;
+  }
+
+  public function setStepResults(array $step_results) {
+    $this->step_results = $step_results;
+  }
+
+  public function getResultByStep($stage, $step) {
+    return $this->step_results[$stage][$step];
+  }
+
+  public function setResultByStep($stage, $step, $result) {
+    $this->step_results[$stage][$step] = $result;
+  }
+
+  // Stores the console output for each build process step within the job
+  protected $step_output;
+
+  public function getStepOutput() {
+    return $this->step_output;
+  }
+
+  public function setStepOutput(array $step_output) {
+    $this->step_output = $step_output;
+  }
+
+  public function getOutputByStep($stage, $step) {
+    return $this->step_output[$stage][$step];
+  }
+
+  public function setOutputByStep($stage, $step, $output) {
+    $this->step_output[$stage][$step] = $output;
+  }
+
+  // Stores a list of job artifacts to be considered part of the final results for this job
   protected $artifacts;
-  public function setArtifacts($artifacts) { $this->artifacts = $artifacts; }
-  public function getArtifacts() { return $this->artifacts; }
 
+  public function setArtifacts($artifacts) {
+    $this->artifacts = $artifacts;
+  }
+
+  public function getArtifacts() {
+    return $this->artifacts;
+  }
+
+  // Stores a list of publishers which should be notified of the results upon job completion
   protected $publishers = [];
-  public function getPublishers() {  return $this->publishers;  }
-  public function setPublishers($publishers) {  $this->publishers = $publishers;  }
-  public function getPublisher($publisher) {  return $this->publishers[$publisher];  }
+
+  public function getPublishers() {
+    return $this->publishers;
+  }
+
+  public function setPublishers($publishers) {
+    $this->publishers = $publishers;
+  }
+
+  public function getPublisher($publisher) {
+    return $this->publishers[$publisher];
+  }
 
 
   public function __construct(JobInterface $job) {
@@ -76,7 +165,7 @@ class JobResults {
 
   // TODO: Consider adding a 'job publisher' class for interim feedback and/or real-time display
   /*
-    // Pasting this code here for future reference, once we revisit interacting with a results API.
+  // Pasting this code here for future reference, once we revisit interacting with a results API.
 
   public function prepServerForPublishing(JobDefinition $jobDefinition) {
     // If we are publishing this job to a results server (or multiple), prep the server
