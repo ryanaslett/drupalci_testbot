26ee183d0428370468a64bf57aaa1869e0d80f56
Jeremy Thorson <jthorson@sasktel.net>
2016-02-26 23:24:51 -0600

    Issue #2642636 by jthorson: Introduce LoggerProvider

--- src/DrupalCI/Console/Command/DrupalCICommandBase.php
+++ src/DrupalCI/Console/Command/DrupalCICommandBase.php
@@ -9,8 +9,10 @@ namespace DrupalCI\Console\Command;
 
 use DrupalCI\Console\Output;
 use DrupalCI\Providers\DockerServiceProvider;
+use DrupalCI\Providers\LoggerProvider;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -29,6 +31,13 @@ class DrupalCICommandBase extends SymfonyCommand {
   protected $container;
 
   /**
+   * Our logger object.
+   *
+   * @var ConsoleLogger
+   */
+  protected $logger;
+
+  /**
    * {@inheritdoc}
    */
   protected function initialize(InputInterface $input, OutputInterface $output) {
@@ -36,8 +45,9 @@ class DrupalCICommandBase extends SymfonyCommand {
     // Perform some container set-up before command execution.
     $this->container = $this->getApplication()->getContainer();
     $this->container->register(new ConsoleOutputServiceProvider($output));
+    $this->container->register(new LoggerProvider());
+    $this->logger = $this->container['logger'];
   }
-
 
   // Defaults for the underlying commands i.e. when commands run with --no-interaction or
   // when we are given options to setup containers.
