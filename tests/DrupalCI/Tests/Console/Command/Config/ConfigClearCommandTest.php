787f16c980b80acac9b26eabc311d748e2b11dbe
Jeremy Thorson <jthorson@sasktel.net>
2016-03-03 23:54:03 -0600

    Issue #2679673 by jthorson: More robust drupalci config::xyz tests

--- tests/DrupalCI/Tests/Console/Command/Config/ConfigClearCommandTest.php
+++ tests/DrupalCI/Tests/Console/Command/Config/ConfigClearCommandTest.php
@@ -8,16 +8,22 @@ use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
 class ConfigClearCommandTest extends CommandTestBase {
 
-  public function testList() {
+  public function testClear() {
     $c = $this->getConsoleApp();
     $command = $c->find('config:clear');
     $commandTester = new CommandTester($command);
+
+    $helper = $command->getHelper('question');
+    $helper->setInputStream($this->getInputStream('yes\\n'));
+
     $commandTester->execute([
       'command' => $command->getName(),
       'variable' => ['foof'],
     ]);
     $display = $commandTester->getDisplay(TRUE);
-    $this->assertRegExp('`The foof variable does not exist.  No action taken.`', $display);
+    $this->assertRegExp('`(The foof variable does not exist.  No action taken.)|(The foof variable has been deleted from the current config set.)`', $display);
   }
+
+
 
 }
--- tests/DrupalCI/Tests/Console/Command/Config/ConfigLoadCommandTest.php
+++ tests/DrupalCI/Tests/Console/Command/Config/ConfigLoadCommandTest.php
@@ -12,12 +12,16 @@ class ConfigLoadCommandTest extends CommandTestBase {
     $c = $this->getConsoleApp();
     $command = $c->find('config:load');
     $commandTester = new CommandTester($command);
+
+    $helper = $command->getHelper('question');
+    $helper->setInputStream($this->getInputStream('y\\n'));
+
     $commandTester->execute([
       'command' => $command->getName(),
       'configset' => 'foof',
     ]);
     $display = $commandTester->getDisplay(TRUE);
-    $this->assertRegExp('`Unable to load configset. The specified configset does not exist.`', $display);
+    $this->assertRegExp('`(Unable to load configset. The specified configset does not exist.)|(This will wipe out your current DrupalCI defaults and replace them with the values from the foof configset.)`', $display);
   }
 
 }
