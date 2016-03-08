16ba85faa52fe0c0bf2d945ccf5540d102ff9c2e
Paul Mitchum <paul@mile23.com>
2016-03-08 13:47:46 -0800

    Issue #2642636: More stability for RunCommandTest.

--- tests/DrupalCI/Tests/Console/Command/RunCommandTest.php
+++ tests/DrupalCI/Tests/Console/Command/RunCommandTest.php
@@ -2,13 +2,22 @@
 
 namespace DrupalCI\Tests\Console\Command;
 
+use DrupalCI\Console\Helpers\ConfigHelper;
 use DrupalCI\Tests\Console\Command\CommandTestBase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
 class RunCommandTest extends CommandTestBase {
 
-  public function testStatus() {
+  public function testRun() {
+    // Load the blank configset.
+    // @todo: Convert this test to DrupalCIFunctionalTestBase after
+    //   https://www.drupal.org/node/2683013
+    $config_helper = new ConfigHelper();
+    if (!$config_helper->activateConfig('blank')) {
+      throw new \PHPUnit_Framework_Exception('Unable to load blank configset.');
+    }
+
     $c = $this->getConsoleApp();
     $command = $c->find('run');
     $commandTester = new CommandTester($command);
