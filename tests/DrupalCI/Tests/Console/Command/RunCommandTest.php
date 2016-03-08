2f8eab5a42df3d0edaaf17d55fcdf21be3db7791
Mile23 <Mile23@116231.no-reply.drupal.org>
2016-03-07 16:38:09 -0800

    Issue #2683013 by Mile23: Better functional test framework

--- tests/DrupalCI/Tests/Console/Command/RunCommandTest.php
+++ tests/DrupalCI/Tests/Console/Command/RunCommandTest.php
@@ -5,9 +5,8 @@ namespace DrupalCI\Tests\Console\Command;
 use DrupalCI\Console\Helpers\ConfigHelper;
 use DrupalCI\Tests\Console\Command\CommandTestBase;
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
-class RunCommandTest extends CommandTestBase {
+class RunCommandTest extends DrupalCIFunctionalTestBase {
 
   public function testRun() {
     // Load the blank configset.
@@ -21,17 +20,10 @@ class RunCommandTest extends CommandTestBase {
     $c = $this->getConsoleApp();
     $command = $c->find('run');
     $commandTester = new CommandTester($command);
-    try {
-      $commandTester->execute(['command' => $command->getName()]);
-    }
-    catch (FileNotFoundException $e) {
-      $display = $commandTester->getDisplay(TRUE);
-      $this->assertRegExp('`Executing job with build ID:`', $display);
-      $this->assertRegExp('`Loading DrupalCI platform default arguments:`', $display);
-      $this->assertRegExp('`Using job definition template: ./drupalci.yml`', $display);
-      return;
-    }
-    $this->fail('Run command did not throw exception or display status info.');
+    $commandTester->execute(['command' => $command->getName()]);
+    $display = $commandTester->getDisplay(TRUE);
+    $this->assertRegExp('`Executing job with build ID:`', $display);
+    $this->assertRegExp('`Loading DrupalCI platform default arguments:`', $display);
   }
 
 }
