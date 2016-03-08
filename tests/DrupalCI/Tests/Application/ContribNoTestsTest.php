4bf7f5ca08a9329a678db50c6a86286c88b01bad
Paul Mitchum <paul@mile23.com>
2016-03-08 11:42:12 -0800

    Issue #2683013: Added contrib-with-no-tests test. Moved base class to the top of namespace hierarchy.

--- tests/DrupalCI/Tests/Application/ContribNoTestsTest.php
+++ tests/DrupalCI/Tests/Application/ContribNoTestsTest.php
@@ -6,7 +6,10 @@ use DrupalCI\Tests\DrupalCIFunctionalTestBase;
 use Symfony\Component\Console\Tester\ApplicationTester;
 
 /**
- * Test what happens when a patch fails to apply properly.
+ * Test what happens a contrib module has no tests.
+ *
+ * This test depends on the drupalci_d8_no_tests module which you can find here:
+ * https://www.drupal.org/sandbox/mile23/2683655
  *
  * NOTE: This test assumes you have checked out Drupal 8.1.x branch into a
  * directory called /tmp/drupal, using a command like this:
@@ -16,21 +19,22 @@ use Symfony\Component\Console\Tester\ApplicationTester;
  *
  * @see TESTING.md
  */
-class PatchFailTest extends DrupalCIFunctionalTestBase {
+class ContribNoTestsTest extends DrupalCIFunctionalTestBase {
 
   /**
    * {@inheritdoc}
    */
   protected $dciConfig = [
     'DCI_UseLocalCodebase=/tmp/drupal',
-    'DCI_JobType=simpletest',
     'DCI_CoreBranch=8.1.x',
-    'DCI_TestGroups=ban',
-    'DCI_Fetch=http://drupal.org/files/issues/add_new_file.patch',
-    'DCI_Patch=add_new_file.patch',
+    'DCI_AdditionalRepositories=git,https://git.drupal.org/sandbox/Mile23/2683655.git,8.x-1.x,modules/drupalci_d8_module_no_tests,1;',
+    'DCI_DBVersion=mysql-5.5',
+    'DCI_PHPVersion=5.5',
+    'DCI_JobType=simpletest',
+    'DCI_TestItem=directory:modules/drupalci_d8_module_no_tests',
   ];
 
-  public function testBadPatch() {
+  public function testContribNoTests() {
     $app = $this->getConsoleApp();
     $options = ['interactive' => FALSE];
 
@@ -39,8 +43,8 @@ class PatchFailTest extends DrupalCIFunctionalTestBase {
       'command' => 'run',
     ], $options);
 
-    $this->assertRegExp('/Patch.*applied to directory.*/', $app_tester->getDisplay());
-    $this->assertNotEquals(0, $app_tester->getStatusCode());
+    $this->assertRegExp('/ERROR: No valid tests were specified./', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
   }
 
 }
