095fae8239929a507e1a8c7845c6f20a13998237
Jeremy Thorson <jthorson@sasktel.net>
2016-03-16 13:53:16 -0600

    by jthorson: Updates to ContainerCommandTest

--- tests/DrupalCI/Tests/Plugin/BuildSteps/generic/ContainerCommandTest.php
+++ tests/DrupalCI/Tests/Plugin/BuildSteps/generic/ContainerCommandTest.php
@@ -29,27 +29,51 @@ class ContainerCommandTest extends DrupalCITestCase {
       ->disableOriginalConstructor()
       ->setMethods(['getExecManager', 'getContainerManager'])
       ->getMock();
-    $container_manager = $this->getMockBuilder('Docker\Manager\ContainerManager')
-      ->disableOriginalConstructor()
+
+    $job = $this->getMockBuilder('DrupalCI\Plugin\JobTypes\JobInterface')
+      ->getMockForAbstractClass();
+    $job->expects($this->once())
+      ->method('getDocker')
+      ->will($this->returnValue($docker));
+    $job->expects($this->once())
+      ->method('getExecContainers')
+      ->will($this->returnValue(['php' => [['id' => 'drupalci/php-5.4']]]));
+
+    $exec_config = $this->getMockBuilder('Docker\API\Model\ExecConfig')
+      ->setMethods(['setTty', 'SetAttachStderr', 'setAttachStdout', 'setAttachStdin', 'setCmd'])
       ->getMock();
+    $exec_config->expects($this->once())
+      ->method('setTty')
+      ->will($this->returnValue($this->returnSelf()));
+    $exec_config->expects($this->once())
+      ->method('setAttachStderr')
+      ->will($this->returnValue($this->returnSelf()));
+    $exec_config->expects($this->once())
+      ->method('setAttachStdout')
+      ->will($this->returnValue($this->returnSelf()));
+    $exec_config->expects($this->once())
+      ->method('setAttachStdin')
+      ->will($this->returnValue($this->returnSelf()));
+    $exec_config->expects($this->once())
+      ->method('setCmd');
+
     $exec_manager = $this->getMockBuilder('Docker\Manager\ExecManager')
       ->disableOriginalConstructor()
       ->setMethods(['create', 'start', 'find'])
       ->getMock();
-    $docker->expects($this->once())
-      ->method('getContainerManager')
-      ->will($this->returnValue($container_manager));
     $docker->expects($this->once())
       ->method('getExecManager')
       ->will($this->returnValue($exec_manager));
 
     $exec_result = $this->getMock('Docker\API\Model\ExecCreateResult');
 
-    $exec_result->expects($this->once())
-      ->method('getId');
     $exec_manager->expects($this->once())
       ->method('create')
       ->will($this->returnValue($exec_result));
+    $exec_result->method('getId')
+      ->willReturn('abcdef');
+    $exec_result->expects($this->once())
+      ->method('getId');
 
     $exec_start_config = $this->getMockBuilder('Docker\API\Model\ExecStartConfig')
       ->setMethods(['setTty', 'setDetach'])
@@ -67,7 +91,7 @@ class ContainerCommandTest extends DrupalCITestCase {
 
     $exec_manager->expects($this->once())
       ->method('start')
-      ->with($exec_start_config)    // $exec_start_config is the second parameter, need a string for $id
+      ->with($exec_result->getId(), $exec_start_config)
       ->will($this->returnValue($stream));
 
     $exec_command = $this->getMockBuilder('Docker\API\Model\ExecCommand')
@@ -79,14 +103,7 @@ class ContainerCommandTest extends DrupalCITestCase {
       ->method('find')
       ->will($this->returnValue($exec_command));
 
-    $job = $this->getMockBuilder('DrupalCI\Plugin\JobTypes\JobInterface')
-      ->getMockForAbstractClass();
-    $job->expects($this->once())
-      ->method('getDocker')
-      ->will($this->returnValue($docker));
-    $job->expects($this->once())
-      ->method('getExecContainers')
-      ->will($this->returnValue(['php' => [['id' => 'drupalci/php-5.4']]]));
+
 
     $command = new ContainerCommand();
     $command->run($job, $cmd);
