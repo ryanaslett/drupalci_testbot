4c747fe1e55a3668f18644b01118e886037a944d
Archie Brentano <isntall.us@gmail.com>
2016-04-20 15:12:13 -0700

    by isntall: adding matrix testing for Drupal 7

---
+++ tests/DrupalCI/Tests/Application/PassingSimpletestLegacy7Test.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Basic test that proves that drupalci can execute a simpletest and generate a result
+ *
+ * NOTE: This test assumes you have checked out Drupal 7.x branch into a
+ * directory called /tmp/drupal7, using a command like this:
+ * git clone --branch 7.x https://git.drupal.org/project/drupal.git
+ *
+ * @group Application
+ * @group docker
+ *
+ * @see TESTING.md
+ */
+class PassingSimpletestLegacy7Test extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  protected $dciConfig = [
+    'DCI_UseLocalCodebase=/tmp/drupal7',
+    'DCI_JobType=simpletestlegacy7',
+    'DCI_CoreBranch=7.x',
+    'DCI_RunScript=/var/www/html/scripts/run-tests.sh',
+    'DCI_TestGroups=Blog',
+    'DCI_JunitXml=xml',
+  ];
+
+  private $dciConfigPhpVer = [
+    'DCI_PHPVersion=5.3',
+    'DCI_PHPVersion=5.4',
+  ];
+  private $dciConfigDb = [
+    'DCI_DBVersion=mysql-5.5',
+  ];
+
+  public function testBasicTest() {
+    foreach ($this->dciConfigDb as $dbKey) {
+      array_push($this->dciConfig, $dbKey);
+      foreach ($this->dciConfigPhpVer as $phpKey) {
+        array_push($this->dciConfig, $phpKey);
+        $this->setUp();
+
+        $app = $this->getConsoleApp();
+        $options = ['interactive' => FALSE];
+
+        $app_tester = new ApplicationTester($app);
+        $app_tester->run([
+          'command' => 'run',
+        ], $options);
+
+        $display = $app_tester->getDisplay();
+        $job = $this->getCommand('run')->getJob();
+
+        $this->assertRegExp('/.*Blog functionality 244 passes, 0 fails, and 0 exceptions*/', $app_tester->getDisplay());
+        // Look for junit xml results file
+        $output_file = $job->getJobCodebase()->getWorkingDir() . "/artifacts/" . $job->getBuildVars()["DCI_JunitXml"] . '/testresults.xml';
+        $this->assertFileExists($output_file);
+
+        // create a test fixture that contains the xml output results.
+        //$this->assertFileEquals();
+        $this->assertXmlFileEqualsXmlFile(__DIR__ . '/Fixtures/PassingSimpletestLegacy7Test_testresults.xml', $output_file);
+
+        array_pop($this->dciConfig);
+      }
+
+      array_pop($this->dciConfig);
+    }
+  }
+}
---
+++ tests/DrupalCI/Tests/Console/Helpers/ConfigHelperTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * DrupalCI Config helper class.
+ */
+
+namespace DrupalCI\Tests\Console\Helpers;
+
+use DrupalCI\Console\Helpers\ConfigHelper;
+use DrupalCI\Tests\DrupalCITestCase;
+
+class ConfigHelperTest extends DrupalCITestCase {
+
+  private $origHome;
+  private $tmp_home;
+  private $dci_config;
+  private $data = [
+    'DCI_PHPVersion' => '7',
+    'DCI_Concurrency' => '2',
+    'DCI_CoreRepository' => 'git://git.drupal.org/project/drupal.git',
+    'DCI_TestGroups' => 'Url',
+  ];
+
+  public function setUp(){
+    $this->tmp_home = "/tmp/confighelpertest";
+    putenv('HOME='.$this->tmp_home);
+    mkdir($this->tmp_home . '/.drupalci', 0775, true);
+    $this->dci_config = $this->tmp_home . '/.drupalci/config';
+    copy(__DIR__ . '/Fixtures/ConfigHelperTest_config', $this->dci_config);
+    foreach ($this->data as $key => $value) {
+      $_ENV[$key] = $value;
+    }
+  }
+
+  public function testGetCurrentEnvVars() {
+
+    $confighelper = new ConfigHelper();
+    $results = $confighelper->getCurrentEnvVars();
+
+    foreach ($this->data as $key => $value) {
+      # code...
+      $this->assertEquals( $value, $results[$key]);
+    }
+  }
+
+  public function testGetCurrentConfigSetContents() {
+    $confighelper = new ConfigHelper();
+    $results = $confighelper->getCurrentConfigSetContents();
+    $r2 = '';
+    foreach ($results as $key => $value) {
+      $r2 .= $value . PHP_EOL;
+    }
+
+    $this->assertFileExists($this->dci_config);
+    $this->assertStringEqualsFile(__DIR__ . '/Fixtures/ConfigHelperTest_config', $r2, 'The file has been altered.');
+
+  }
+
+  public function testGetCurrentConfigSetParsed() {
+    $confighelper = new ConfigHelper();
+    $results = $confighelper->getCurrentConfigSetParsed();
+    $this->assertEquals($results['DCI_PHPVersion'], $this->data['DCI_PHPVersion']);
+    $this->assertEquals($results['DCI_Concurrency'], $this->data['DCI_Concurrency']);
+    $this->assertEquals($results['DCI_TestGroups'], $this->data['DCI_TestGroups']);
+  }
+
+
+  protected function tearDown(){
+    unlink($this->tmp_home . '/.drupalci/config');
+    rmdir($this->tmp_home . '/.drupalci');
+    rmdir($this->tmp_home);
+//    putenv('HOME=' . $this->$origHome);
+  }
+}
+//Class TestConfigHelper extends ConfigHelper {
+//  use TestSetupBaseTrait;
+//}
