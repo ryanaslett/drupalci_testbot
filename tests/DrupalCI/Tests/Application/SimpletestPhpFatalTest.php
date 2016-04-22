e2b3c203e59761bbf1bb13a4205f4c96eb7b4c47
isntall <isntall@2899677.no-reply.drupal.org>
2016-04-22 13:21:16 -0700

    Issue #2680565 by isntall: Create functional tests of run command output to test production essentials

---
+++ tests/DrupalCI/Tests/Application/SimpletestPhpFatalTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Test what happens a contrib module has no tests.
+ *
+ * This test depends on the drupalci_d8_no_tests module which you can find here:
+ * https://www.drupal.org/sandbox/mile23/2683655
+ *
+ * NOTE: This test assumes you have checked out Drupal 8.1.x branch into a
+ * directory called /tmp/drupal, using a command like this:
+ * git clone --branch 8.1.x https://git.drupal.org/project/drupal.git
+ *
+ * @group Application
+ *
+ * @see TESTING.md
+ */
+class simpletestPhpFatalTest extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dciConfig = [
+    'DCI_CoreRepository=file:///tmp/drupal.git',
+    'DCI_RunScript=/var/www/html/core/scripts/run-tests.sh ',
+    'DCI_PHPVersion=5.5',
+    'DCI_JobType=simpletest',
+    'DCI_ComposerInstall=True',
+    'DCI_CoreBranch=8.1.x',
+    'DCI_Patch=2684095-2.patch,.',
+    'DCI_Color=True',
+    'DCI_JunitXml=xml',
+    'DCI_Fetch=https://www.drupal.org/files/issues/2684095-2.patch,.',
+    'DCI_Concurrency=2',
+    'DCI_DBVersion=mysql-5.5',
+    'DCI_GitCommitHash=6afe359',
+    'DCI_TestGroups=--class "Drupal\comment\Tests\CommentItemTest"',
+  ];
+
+  public function testSimpletestPhpFatal() {
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+    ], $options);
+    $this->assertRegExp('/Fatal error/', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
+  }
+}
