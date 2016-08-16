9d2b3d12349175d1be6abf57aa98b47cc012d707
Mixologic <ryan+ddoto@association.drupal.org>
2016-08-16 15:43:31 -0700

    Eliminates unnecessary container checking from d7 simple passing test result.

--- tests/DrupalCI/Tests/Application/PassingSimpletestLegacy7Test.php
+++ tests/DrupalCI/Tests/Application/PassingSimpletestLegacy7Test.php
@@ -28,43 +28,32 @@ class PassingSimpletestLegacy7Test extends DrupalCIFunctionalTestBase {
     'DCI_CoreRepository=file:///tmp/drupal',
     'DCI_JobType=simpletestlegacy7',
     'DCI_JunitXml=xml',
+    'DCI_GitCommitHash=3d5bcd3',
     'DCI_RunScript=/var/www/html/scripts/run-tests.sh',
     'DCI_TestGroups=Blog',
-  ];
-
-  private $dciConfigPhpVer = [
     'DCI_PHPVersion=5.3',
-    'DCI_PHPVersion=5.4',
-  ];
-  private $dciConfigDb = [
     'DCI_DBVersion=mysql-5.5',
   ];
 
   public function testBasicTest() {
-    foreach ($this->dciConfigDb as $dbKey) {
-      array_push($this->dciConfig, $dbKey);
-      foreach ($this->dciConfigPhpVer as $phpKey) {
-        array_push($this->dciConfig, $phpKey);
-        $this->setUp();
-        $app = $this->getConsoleApp();
-        $options = ['interactive' => FALSE];
-        $app_tester = new ApplicationTester($app);
-        $app_tester->run([
-          'command' => 'run',
-        ], $options);
-        $display = $app_tester->getDisplay();
-        $job = $this->getCommand('run')->getJob();
-        $this->assertRegExp('/.*simpletestlegacy7*/', $app_tester->getDisplay());
-        $this->assertRegExp('/.*Blog functionality 244 passes, 0 fails, and 0 exceptions*/', $app_tester->getDisplay());
-        // Look for junit xml results file
-        $output_file = $job->getJobCodebase()->getWorkingDir() . "/artifacts/" . $job->getBuildVars()["DCI_JunitXml"] . '/testresults.xml';
-        $this->assertFileExists($output_file);
-        // create a test fixture that contains the xml output results.
-        //$this->assertFileEquals();
-        $this->assertXmlFileEqualsXmlFile(__DIR__ . '/Fixtures/PassingSimpletestLegacy7Test_testresults.xml', $output_file);
-        array_pop($this->dciConfig);
-      }
-      array_pop($this->dciConfig);
-    }
+
+    $this->setUp();
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+    ], $options);
+    $job = $this->getCommand('run')->getJob();
+    $this->assertRegExp('/.*simpletestlegacy7*/', $app_tester->getDisplay());
+    $this->assertRegExp('/.*Blog functionality 244 passes, 0 fails, and 0 exceptions*/', $app_tester->getDisplay());
+    // Look for junit xml results file
+    $output_file = $job->getJobCodebase()
+        ->getWorkingDir() . "/artifacts/" . $job->getBuildVars()["DCI_JunitXml"] . '/testresults.xml';
+    $this->assertFileExists($output_file);
+    // create a test fixture that contains the xml output results.
+    //$this->assertFileEquals();
+    $this->assertXmlFileEqualsXmlFile(__DIR__ . '/Fixtures/PassingSimpletestLegacy7Test_testresults.xml', $output_file);
+
   }
 }
