869a4f086170521d8be721906925eef80a91983a
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-10-26 15:45:45 -0700

    Issue #2820540: Updates the BuildTask Interface, and adds a BuildTaskTrait that allows us to track timing information of steps along the way

--- src/DrupalCI/Plugin/BuildTask/BuildTaskTrait.php
+++ src/DrupalCI/Plugin/BuildTask/BuildTaskTrait.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace DrupalCI\Plugin;
+namespace DrupalCI\Plugin\BuildTask;
 
-use DrupalCI\Plugin\BuildTaskInterface;
+use DrupalCI\Build\BuildInterface;
+use DrupalCI\Plugin\BuildTask;
+use DrupalCI\Plugin\BuildTask\BuildTaskInterface;
+use DrupalCI\Plugin\type;
 
 /**
  * Support cascading config resolution in plugins.
@@ -17,6 +20,33 @@ trait BuildTaskTrait {
    * @var \DrupalCI\Build\BuildVariablesInterface
    */
   protected $buildVars;
+
+  /**
+   * @var float
+   */
+  protected $startTime;
+
+  /**
+   * @var float
+   *   Total time taken for this build task, including child tasks
+   */
+  protected $elapsedTime;
+
+  /**
+   * Any variables that can affect the behavior of this plugin, that are
+   * specific to this plugin, reside in a configuration array within the plugin.
+   *
+   * @var array
+   *
+   */
+  protected $configuration;
+
+  /**
+   * Configuration overrides passed into the plugin.
+   *
+   * @var array
+   */
+  protected $configuration_overrides;
 
   /**
    * Performs substitution on DCI_* variables within config.
@@ -63,4 +93,39 @@ trait BuildTaskTrait {
     }
   }
 
+  /**
+   * Decorator for run functions to allow all of them to be timed.
+   *
+   * @param \DrupalCI\Build\BuildInterface $build
+   */
+  public function start(BuildInterface $build) {
+    $this->startTime = microtime(true);
+    $this->run($build);
+  }
+
+  /**
+   * Decorator for complete functions to stop their timer.
+   */
+  public function finish() {
+    $this->complete();
+    $elapsed_time = microtime(true) - $this->startTime;
+    $this->elapsedTime = $elapsed_time;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getElapsedTime($inclusive = TRUE) {
+    return $this->elapsedTime;
+  }
+
+  protected function override_config() {
+
+    if (!empty($this->configuration_overrides)) {
+      if ($invalid_overrides = array_diff_key($this->configuration_overrides, $this->configuration)){
+        // @TODO: somebody is trying to override a non-existant configuration value. Throw an exception? print a warning?
+      }
+      $this->configuration = array_merge($this->configuration, array_intersect_key($this->configuration_overrides, $this->configuration));
+    }
+  }
 }
