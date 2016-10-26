8cd732543cd5211cfb025ae1cff63ca14ee1532b
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-10-26 15:53:20 -0700

    Issue #2820540: Updates pluginbase to fix the constructor and provide the exec wrapper to all subordinate plugins.

--- src/DrupalCI/Plugin/PluginBase.php
+++ src/DrupalCI/Plugin/PluginBase.php
@@ -7,12 +7,16 @@
 namespace DrupalCI\Plugin;
 
 use DrupalCI\Build\BuildInterface;
+use DrupalCI\Plugin\BuildTask\BuildTaskTrait;
 
 /**
  * Base class for plugins.
  */
 abstract class PluginBase {
 
+  // TODO: Perhaps this isnt BuildTaskTrait, but a PluginTrait that figures out
+  // configuration?
+  use BuildTaskTrait;
   /**
    * The plugin_id.
    *
@@ -28,33 +32,27 @@ abstract class PluginBase {
   protected $pluginDefinition;
 
   /**
-   * Configuration information passed into the plugin.
-   *
-   * When using an interface like
-   * \Drupal\Component\Plugin\ConfigurablePluginInterface, this is where the
-   * configuration should be stored.
-   *
-   * Plugin configuration is optional, so plugin implementations must provide
-   * their own setters and getters.
-   *
-   * @var array
-   */
-  protected $configuration;
-
-  /**
    * Constructs a Drupal\Component\Plugin\PluginBase object.
    *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
+   * @param array $configuration_overrides
+   *   A configuration array containing overrides from the build.yml file.
    * @param string $plugin_id
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
    */
-  public function __construct(array $configuration = [], $plugin_id = '', $plugin_definition = []) {
-    $this->configuration = $configuration;
+  public function __construct(array $configuration_overrides = [], $plugin_id = '', $plugin_definition = []) {
+    $this->configuration = $this->getDefaultConfiguration();
+    $this->configuration_overrides = $configuration_overrides;
     $this->pluginId = $plugin_id;
     $this->pluginDefinition = $plugin_definition;
+    // Compute the plugin's configuration.
+    $this->configure();
+    $this->override_config();
+  }
+
+  protected function exec($command, &$output, &$return_var) {
+    exec($command, $output, $return_var);
   }
 
 }
